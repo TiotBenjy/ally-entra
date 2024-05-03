2d66e7f5ac9e3b91194ef1fa5ca646382e998156
TiotBenjy <66011955+TiotBenjy@users.noreply.github.com>
2024-05-03 07:53:39 +0200

    feat(ally): Setup service definition

--- index.ts
+++ index.ts
@@ -1,2 +1,9 @@
 export { stubsRoot } from './stubs/main.js'
 export { configure } from './configure.js'
+import { EntraIdDriver } from './src/entra_id.js'
+import type { EntraIdDriverConfig } from './src/types/main.js'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export function entraId(config: EntraIdDriverConfig) {
+  return (ctx: HttpContext) => new EntraIdDriver(ctx, config)
+}
